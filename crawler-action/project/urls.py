3f33b352dd19a72c27a4341d0bfd8ef12c296696
RA <numpde@null.net>
2024-03-15 16:30:05 +0300

    add slashes

--- crawler-action/project/urls.py
+++ crawler-action/project/urls.py
@@ -4,7 +4,7 @@ from app.views import MarkdownView, PlainView, GPTView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/view/md', MarkdownView.as_view(), name='content'),
-    path('api/view/md/gpt', GPTView.as_view(), name='content'),
-    path('api/view/plain', PlainView.as_view(), name='content'),
+    path('api/view/md/', MarkdownView.as_view(), name='content'),
+    path('api/view/md/gpt/', GPTView.as_view(), name='content'),
+    path('api/view/plain/', PlainView.as_view(), name='content'),
 ]
